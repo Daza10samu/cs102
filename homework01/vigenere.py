c01fb63231e9e4a66e4f74e0fe8fdfdc999e6f9f
Daza10Samu <andreyhigher@gmail.com>
2020-09-26 12:20:30 +0300

    Complete vigenere.py

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -10,7 +10,26 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    keyword_len = len(keyword)
+    shifts = []
+    for char in keyword:
+        if char.islower():
+            shifts.append(ord(char) - 97)
+        else:
+            shifts.append(ord(char) - 65)
+    for index, char in enumerate(plaintext):
+        if not char.isalpha():
+            ciphertext += char
+            continue
+        char_index = ord(char)
+        is_lower = char.islower()
+        if is_lower:
+            alphabet_start_number = 97
+        else:
+            alphabet_start_number = 65
+        letter_number = char_index - alphabet_start_number
+        cipher_number = (letter_number + shifts[index % keyword_len]) % 26
+        ciphertext += chr(cipher_number + alphabet_start_number)
     return ciphertext
 
 
@@ -26,5 +45,24 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    keyword_len = len(keyword)
+    shifts = []
+    for char in keyword:
+        if char.islower():
+            shifts.append(ord(char) - 97)
+        else:
+            shifts.append(ord(char) - 65)
+    for index, char in enumerate(ciphertext):
+        if not char.isalpha():
+            plaintext += char
+            continue
+        char_index = ord(char)
+        is_lower = char.islower()
+        if is_lower:
+            alphabet_start_number = 97
+        else:
+            alphabet_start_number = 65
+        cipher_number = char_index - alphabet_start_number
+        letter_number = (26 + cipher_number - shifts[index % keyword_len]) % 26
+        plaintext += chr(letter_number + alphabet_start_number)
     return plaintext
