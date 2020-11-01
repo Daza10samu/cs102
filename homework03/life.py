676632939a8ac81149e01db000c3ff17a5a08344
Daza10Samu <andreyhigher@gmail.com>
2020-11-01 14:37:56 +0300

    main class completed

--- homework03/life.py
+++ homework03/life.py
@@ -2,9 +2,6 @@ import pathlib
 import random
 import typing as tp
 
-import pygame
-from pygame.locals import *
-
 Cell = tp.Tuple[int, int]
 Cells = tp.List[int]
 Grid = tp.List[Cells]
@@ -12,10 +9,10 @@ Grid = tp.List[Cells]
 
 class GameOfLife:
     def __init__(
-        self,
-        size: tp.Tuple[int, int],
-        randomize: bool = True,
-        max_generations: tp.Optional[float] = float("inf"),
+            self,
+            size: tp.Tuple[int, int],
+            randomize: bool = True,
+            max_generations: tp.Optional[float] = float("inf"),
     ) -> None:
         # Размер клеточного поля
         self.rows, self.cols = size
@@ -30,45 +27,155 @@ class GameOfLife:
 
     def create_grid(self, randomize: bool = False) -> Grid:
         # Copy from previous assignment
-        pass
+        if randomize:
+            return [[random.randint(0, 1) for _ in range(self.cols)] for _ in range(self.rows)]
+        else:
+            return [[0] * self.cols for _ in range(self.rows)]
 
     def get_neighbours(self, cell: Cell) -> Cells:
         # Copy from previous assignment
-        pass
+        if self.cols == 1 and self.rows == 1:
+            return []
+        elif self.cols == 1 and self.rows != 1:
+            if cell[1] == 0:
+                return [self.curr_generation[0][1]]
+            elif cell[1] == self.rows - 1:
+                return [self.curr_generation[0][cell[1] - 1]]
+            else:
+                return [self.curr_generation[0][cell[1] - 1], self.curr_generation[0][cell[1] + 1]]
+        elif self.cols != 1 and self.rows == 1:
+            if cell[0] == 0:
+                return [self.curr_generation[1][0]]
+            elif cell[0] == self.cols - 1:
+                return [self.curr_generation[cell[0] - 1][0]]
+            else:
+                return [self.curr_generation[cell[0] - 1][0], self.curr_generation[cell[0] + 1][0]]
+        elif cell[0] == 0:
+            if cell[1] == 0:
+                return [
+                    self.curr_generation[1][0],
+                    self.curr_generation[0][1],
+                    self.curr_generation[1][1],
+                ]
+            elif cell[1] == self.cols - 1:
+                return [
+                    self.curr_generation[1][self.cols - 1],
+                    self.curr_generation[0][self.cols - 2],
+                    self.curr_generation[1][self.cols - 2],
+                ]
+            else:
+                return [self.curr_generation[0][j] for j in [cell[1] - 1, cell[1] + 1]] + [
+                    self.curr_generation[1][j] for j in [cell[1] - 1, cell[1], cell[1] + 1]
+                ]
+        elif cell[0] == self.rows - 1:
+            if cell[1] == 0:
+                return [
+                    self.curr_generation[self.rows - 2][0],
+                    self.curr_generation[self.rows - 1][1],
+                    self.curr_generation[self.rows - 2][1],
+                ]
+            elif cell[1] == self.cols - 1:
+                return [
+                    self.curr_generation[self.rows - 2][self.cols - 1],
+                    self.curr_generation[self.rows - 1][self.cols - 2],
+                    self.curr_generation[self.rows - 2][self.cols - 2],
+                ]
+            else:
+                return [
+                           self.curr_generation[self.rows - 1][j] for j in [cell[1] - 1, cell[1] + 1]
+                       ] + [
+                           self.curr_generation[self.rows - 2][j]
+                           for j in [cell[1] - 1, cell[1], cell[1] + 1]
+                       ]
+        else:
+            if cell[1] == 0:
+                return [
+                           self.curr_generation[i][j]
+                           for i in (cell[0] - 1, cell[0] + 1)
+                           for j in range(0, 2)
+                       ] + [self.curr_generation[cell[0]][1]]
+            elif cell[1] == self.cols - 1:
+                return [
+                           self.curr_generation[i][j]
+                           for i in (cell[0] - 1, cell[0] + 1)
+                           for j in range(self.cols - 2, self.cols)
+                       ] + [self.curr_generation[cell[0]][self.cols - 2]]
+            else:
+                return [
+                           self.curr_generation[i][j]
+                           for i in (cell[0] - 1, cell[0] + 1)
+                           for j in range(cell[1] - 1, cell[1] + 2)
+                       ] + [self.curr_generation[cell[0]][j] for j in (cell[1] - 1, cell[1] + 1)]
 
     def get_next_generation(self) -> Grid:
         # Copy from previous assignment
-        pass
+        new_grid = [x.copy() for x in self.curr_generation]
+        for row_id in range(self.rows):
+            for col_id in range(self.cols):
+                alive_count = sum(1 for x in self.get_neighbours((row_id, col_id)) if x == 1)
+                if self.curr_generation[row_id][col_id] == 1 and not (
+                        alive_count == 3 or alive_count == 2
+                ):
+                    new_grid[row_id][col_id] = 0
+                elif self.curr_generation[row_id][col_id] == 0 and alive_count == 3:
+                    new_grid[row_id][col_id] = 1
+        return new_grid
 
     def step(self) -> None:
         """
         Выполнить один шаг игры.
         """
-        pass
+        self.prev_generation, self.curr_generation = (
+            self.curr_generation,
+            self.get_next_generation(),
+        )
+        self.generations += 1
 
     @property
     def is_max_generations_exceeded(self) -> bool:
         """
         Не превысило ли текущее число поколений максимально допустимое.
         """
-        pass
+        return self.generations >= self.max_generations
 
     @property
     def is_changing(self) -> bool:
         """
         Изменилось ли состояние клеток с предыдущего шага.
         """
-        pass
+        return self.prev_generation != self.curr_generation
 
     @staticmethod
     def from_file(filename: pathlib.Path) -> "GameOfLife":
         """
         Прочитать состояние клеток из указанного файла.
         """
-        pass
+        grid = []
+        with filename.open() as f:
+            length = 0
+            for line in f.readlines():
+                line_stripped = line.strip('\n')
+                if length == 0:
+                    length = len(line_stripped)
+                if len(line_stripped) != length or set(line_stripped) != {'0', '1'}:
+                    raise ValueError('Формат файла не подходит')
+                grid.append([])
+                for char in line_stripped:
+                    grid[-1].append(int(char))
+        result_game = GameOfLife(size=(len(grid), length))
+        result_game.curr_generation = grid
+        return result_game
 
-    def save(filename: pathlib.Path) -> None:
+    def save(self, filename: pathlib.Path) -> None:
         """
         Сохранить текущее состояние клеток в указанный файл.
         """
-        pass
+        with filename.open('w') as f:
+            for row in self.curr_generation:
+                for elem in row:
+                    f.write(str(elem))
+                f.write('\n')
+
+
+if __name__ == '__main__':
+    GameOfLife.from_file(pathlib.Path('grid.txt'))
