79474cf2255ca75fa524715b3d686c2c348600cb
Daza10Samu <andreyhigher@gmail.com>
2020-12-28 20:35:04 +0300

    AllDone

--- homework05/vkapi/config.py
+++ homework05/vkapi/config.py
@@ -2,7 +2,6 @@
 
 VK_CONFIG = {
     "domain": "https://api.vk.com/method",
-    # "access_token": "6b20b6412212969f40c7b0bc55568641bd4c2a43506b5fa98173b7a9ad235687961a95137cbf5e3edeb2e",
-    "access_token": "8106651853455779714015ed0971906453b2e020f1f89bfe1bbc4448e435939c4b286817de8fb3578a4a3",
+    "access_token": "",
     "version": "5.126",
 }
