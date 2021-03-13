708fb1730a406ae40889f10fbb3e30c408104e6a
Daza10samu <andreyhigher@gmail.com>
2021-03-13 22:16:20 +0300

    Tests fix

--- homework06/tests/test_classifier.py
+++ homework06/tests/test_classifier.py
@@ -1,8 +1,10 @@
 from classifier import NaiveBayesClassifier
+import pathlib
 
 
 def test_NaiveBayesClassifier():
-    (*data,) = filter(lambda it: it != "", open("tests/SMSSpamCollection").read().split("\n"))
+    with (pathlib.Path(__file__).parent / "SMSSpamCollection").open() as f:
+        (*data,) = filter(lambda it: it != "", f.read().split("\n"))
     (*x,) = map(lambda it: it.split("\t")[1], data)
     (*y,) = map(lambda it: it.split("\t")[0], data)
     x_train, y_train, x_test, y_test = x[:3900], y[:3900], x[3900:], y[3900:]
