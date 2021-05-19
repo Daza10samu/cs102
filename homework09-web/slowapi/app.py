7cf37d5de73c4c4f338ed21839aea7770b6c158f
Daza10samu <andreyhigher@gmail.com>
2021-05-19 23:55:19 +0300

    Done

---
+++ homework09-web/slowapi/app.py
@@ -0,0 +1,86 @@
+import http.client
+import json
+import typing as tp
+from urllib.parse import parse_qsl
+
+from slowapi.request import Request
+from slowapi.response import Response, JsonResponse
+from slowapi.router import Route
+
+
+class SlowAPI:
+    def __init__(self):
+        self.routes: tp.List[Route] = []
+        self.middlewares = []
+
+    def __call__(self, environ, start_response):
+        curr_routes = list(filter(
+            lambda route: environ["PATH_INFO"] == route.path and route.method == environ["REQUEST_METHOD"],
+            self.routes))
+        if len(curr_routes) == 0:
+            curr_routes = list(filter(
+                lambda route: environ["PATH_INFO"].rsplit("/", 1)[0] ==
+                              route.path.rsplit("/", 1)[0] and route.method == environ["REQUEST_METHOD"],
+                self.routes))
+            if len(curr_routes) == 0:
+                raise Exception("Routes error")
+
+        route = curr_routes[0]
+
+        if "{" in route.path:
+            args = environ["PATH_INFO"][route.path.find("{"):].split("&")
+            if len(args) == 1 and args[0] == '':
+                args = []
+        else:
+            args = []
+
+        query_string = environ["QUERY_STRING"]
+        query: tp.Dict[str, str] = dict()
+        for i in query_string.split("&"):
+            if len(i.split("=", 1)) > 1:
+                query[i.split("=", 1)[0]] = i.split("=", 1)[1]
+
+        request = Request(environ["PATH_INFO"], environ["REQUEST_METHOD"], query, environ["wsgi.input"], environ)
+        response = route.func(request, *args)
+        start_response(f"{response.status} {http.client.responses[response.status]}", response.headers)
+
+        if isinstance(response, JsonResponse):
+            return [json.dumps(response.data).encode()]
+        else:
+            return [str(response.body).encode()]
+
+    def route(self, path=None, method=None, **options):
+        self.routes.append(Route(path, method, options["func"]))
+
+    def get(self, path=None, **options):
+        def inner(func):
+            return self.route(path, method="GET", func=func)
+
+        return inner
+
+    def post(self, path=None, **options):
+        def inner(func):
+            return self.route(path, method="POST", func=func)
+
+        return inner
+
+    def patch(self, path=None, **options):
+        def inner(func):
+            return self.route(path, method="PATCH", func=func)
+
+        return inner
+
+    def put(self, path=None, **options):
+        def inner(func):
+            return self.route(path, method="PUT", func=func)
+
+        return inner
+
+    def delete(self, path=None, **options):
+        def inner(func):
+            return self.route(path, method="DELETE", func=func)
+
+        return inner
+
+    def add_middleware(self, middleware) -> None:
+        self.middlewares.append(middleware)
---
+++ homework09-web/slowapi/request.py
@@ -0,0 +1,20 @@
+import dataclasses
+import io
+import json
+import typing as tp
+
+
+@dataclasses.dataclass
+class Request:
+    path: str
+    method: str
+    query: tp.Dict[str, str] = dataclasses.field(default_factory=dict)
+    body: io.BytesIO = dataclasses.field(default_factory=io.BytesIO)
+    headers: tp.Dict[str, str] = dataclasses.field(default_factory=dict)
+
+    def text(self) -> tp.Optional[str]:
+        return self.body.read().decode()
+
+    def json(self) -> tp.Optional[tp.Dict[str, tp.Any]]:
+        return json.loads(self.text())
+
